12aa1e8320873ae9b4e1b94eb60607d1018693d6
venkatakonala <venkata.konala@appdynamics.com>
2017-08-11 10:11:46 -0700

    Changed welcome message

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 class Demo {
 	public static void main(String args[]) {
-		System.out.println("Welcome to AppD pipeline-v2");
+		System.out.println("Welcome to AppD pipeline-v2 with Venkata");
 	}
 }
